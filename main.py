32673739d5376f55fdc6be7c6786bdf83df2af87
Yvan Niyonzima <yvanniyonzima@Yvans-MacBook-Air.local>
2025-02-07 23:33:50 -0500

    using gunicorn for production server

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+@app.get("/test")
+async def test():
+    return {"message": "Hello from test!"}
+
+@app.get("/products")
+async def get_products():
+    return [
+        {"id": 1, "name": "Dog Food", "price": 19.99},
+        {"id": 2, "name": "Cat Food", "price": 34.99},
+        {"id": 3, "name": "Bird Seeds", "price": 10.99},
+    ]
